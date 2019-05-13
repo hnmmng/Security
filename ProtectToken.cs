3cc78d054af339d14b92b5f9c4ddbc52ce185d4d
hnmmng <50571489+hnmmng@users.noreply.github.com>
2019-05-13 13:00:19 +0300

    Cookie

--- ProtectToken.cs
+++ ProtectToken.cs
@@ -1,20 +1,39 @@
- public string ProtectToken(UserContextModel usercontext, HttpContext context)
-        {
-            //Crypto Password to sign user inputs
-            string strPassword = Protect(usercontext.StaffNo + usercontext.StaffId.ToString() + usercontext.Doj);
-            string IV = Protect(usercontext.StaffNo);
-            string saltstr = Protect(usercontext.StaffName);
-            string token, time = DateTime.Now.AddMinutes(usercontext.Expiration).ToString();
-            string ipAddress = Helper.GetIPAddress(context);
-            string guid = Guid.NewGuid().ToString();
-            SetUserContext(usercontext, context);
-            //User token
-            token = ipAddress + "~" + time + "~" + usercontext.StaffId + "~" + guid + "~" + usercontext.StaffNo;
-            return Protect(token) + "|" + strPassword + "|" + IV + "|" + saltstr; //User token encryption
-        }
-        public string Protect(string stringtxt)
-        {
-            var outputData = Helper.ObjectToBase64(stringtxt);
-            var textToOutPut = Encoding.UTF8.GetBytes(outputData);
-            return Convert.ToBase64String(_protector.Protect(textToOutPut));
-        }
+// Generate Token and Crypto Password
+public string ProtectToken(UserContextModel usercontext, HttpContext context)
+ {
+     //Crypto Password to sign user inputs
+     string strPassword = Protect(usercontext.StaffNo + usercontext.StaffId.ToString() + usercontext.Doj);
+     string IV = Protect(usercontext.StaffNo);
+     string saltstr = Protect(usercontext.StaffName);
+     string token, time = DateTime.Now.AddMinutes(usercontext.Expiration).ToString();
+     string ipAddress = Helper.GetIPAddress(context);
+     string guid = Guid.NewGuid().ToString();
+     SetUserContext(usercontext, context);
+     //User token
+     token = ipAddress + "~" + time + "~" + usercontext.StaffId + "~" + guid + "~" + usercontext.StaffNo;
+     return Protect(token) + "|" + strPassword + "|" + IV + "|" + saltstr; //User token encryption
+ }
+ public string Protect(string stringtxt)
+ {
+     var outputData = Helper.ObjectToBase64(stringtxt);
+     var textToOutPut = Encoding.UTF8.GetBytes(outputData);
+     return Convert.ToBase64String(_protector.Protect(textToOutPut));
+ }
+// Set cookie
+public void SetUserContext(UserContextModel userContext, HttpContext context)
+ {
+     if (context.Request.Cookies[SecurityConstant.AuthContext] != null)
+     {
+         context.Response.Cookies.Delete(SecurityConstant.AuthContext);
+     }
+     CookieOptions option = new CookieOptions()
+     {
+         HttpOnly = true,
+         SameSite = SameSiteMode.Strict,
+         Secure= userContext.IsHttps
+     };
+     var outputData = Helper.ObjectToBase64(userContext);
+     var textToOutPut = Encoding.UTF8.GetBytes(outputData);
+     var protectedPayload = Convert.ToBase64String(_protector.Protect(textToOutPut));
+     context.Response.Cookies.Append(SecurityConstant.AuthContext, protectedPayload, option);
+ }
